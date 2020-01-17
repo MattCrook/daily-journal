5f4e0b5cec3d2281a0d7f58f013df7d84120fbdd
Matt Crook <matt.crook11@gmail.com>
2020-01-17 13:15:14 -0600

    feat: add array of objects containing the journal entries. (#2)

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,1 +1,41 @@
-console.log('hello');
+/*
+const journalEntry = {
+  date: "01.17.2020",
+  concepts: "Objects and functions",
+  entry:
+    "Learned the basics of objects, and working with functions as well as methods.",
+  mood: "motivated"
+};
+
+const firstJournalEntry = [];
+firstJournalEntry.push(journalEntry);
+console.log(firstJournalEntry);
+
+const journalEntry = (date, concepts, entry, mood) => {
+  return {
+    date: date,
+    concepts: concepts,
+    entry: entry,
+    mood: mood
+  };
+}
+*/
+
+function journalEntry(date, concepts, entry, mood) {
+    return {
+      date,
+      concepts,
+      entry,
+      mood,
+    }
+}
+const firstJournalEntry = journalEntry("01.17.2020", "Objects and functions", "Learned the basics of objects, and working with functions as well as methods.", "motivated");
+console.log(firstJournalEntry);
+let journalEntriesArray = [];
+journalEntriesArray.push(firstJournalEntry);
+console.log(journalEntriesArray);
+
+
+const secondJournalEntry = journalEntry("01.17.2020", "flexbox", "Worked with CSS to build better styling skills with flexbox.", "neutral");
+journalEntriesArray.push(secondJournalEntry);
+console.log(journalEntriesArray);
