e344748a17fa9f840bbcdde9354c1c8bb77dc18a
Matt Crook <matt.crook11@gmail.com>
2020-01-22 09:35:14 -0600

    fix: still messy but cleaned up destructuring and added funcionality in rendering journal entries to DOM. (#3)

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -20,7 +20,22 @@ const journalEntry = (date, concepts, entry, mood) => {
   };
 }
 */
-
+const journalEntries = [
+  {
+    date: new Date(),
+    concepts: "Objects and functions",
+    entry:
+      "Learned the basics of objects, and working with functions as well as methods.",
+    mood: "motivated"
+  },
+  {
+    date: new Date(),
+    concepts: "flexbox",
+    entry: "Worked with CSS to build better styling skills with flexbox.",
+    mood: "neutral"
+  }
+];
+/*
 function journalEntry(date, concepts, entry, mood) {
     return {
       date,
@@ -29,13 +44,49 @@ function journalEntry(date, concepts, entry, mood) {
       mood,
     }
 }
-const firstJournalEntry = journalEntry("01.17.2020", "Objects and functions", "Learned the basics of objects, and working with functions as well as methods.", "motivated");
+
+const firstJournalEntry = journalEntry(new Date(), "Objects and functions", "Learned the basics of objects, and working with functions as well as methods.", "motivated");
 console.log(firstJournalEntry);
-let journalEntriesArray = [];
-journalEntriesArray.push(firstJournalEntry);
-console.log(journalEntriesArray);
+let journalEntries = [];
+journalEntries.push(firstJournalEntry);
+// console.log(journalEntries);
 
 
-const secondJournalEntry = journalEntry("01.17.2020", "flexbox", "Worked with CSS to build better styling skills with flexbox.", "neutral");
-journalEntriesArray.push(secondJournalEntry);
-console.log(journalEntriesArray);
+const secondJournalEntry = journalEntry(new Date(), "flexbox", "Worked with CSS to build better styling skills with flexbox.", "neutral");
+journalEntries.push(secondJournalEntry);
+console.log(journalEntries);
+*/
+
+/*
+    Purpose: To create, and return, a string template that
+    represents a single journal entry object as HTML
+    Arguments: journalEntry (object)
+*/
+journalEntries.forEach(journalEntry => {
+  const entryLogContainer = document.querySelector(".entryLog");
+  entryLogContainer.innerHTML += makeJournalEntryComponent(journalEntry);
+});
+
+function makeJournalEntryComponent(journalEntry) {  // can also use destructuring
+  // can also use destructuring
+  return `
+    <h1>${journalEntry.concepts}</h1>
+    <h3>${journalEntry.date}</h3>
+    <section>${journalEntry.entry}</section>
+    <section>${journalEntry.mood}</section>
+  `;
+}
+
+
+/*
+    Purpose: To render all journal entries to the DOM
+
+    Arguments: entries (array of objects)
+
+const renderJournalEntries = (entries) => {
+
+}
+
+// Invoke the render function
+renderJournalEntries(journalEntries)
+*/
